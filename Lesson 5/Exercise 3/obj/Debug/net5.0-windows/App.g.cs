9b381521af64a643c3e311bea5be460e7c522717
Windows Progamming <giangcb@gmail.com>
2023-12-28 08:38:54 +0700

    lesson 6

--- Lesson 5/Exercise 3/obj/Debug/net5.0-windows/App.g.cs
+++ Lesson 5/Exercise 3/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Exercise_3 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Exercise_3 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             Exercise_3.App app = new Exercise_3.App();
             app.InitializeComponent();
---
+++ Lesson 6/Exercise 1/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MVVMInWPF.ViewModel;
+using System.Windows;
+
+namespace Exercise_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Exercise_1.View.MainPage window = new Exercise_1.View.MainPage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
